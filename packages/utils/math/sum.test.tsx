dd0df3a85bc8d95d89a342d0b1e31cdd3cbf8d5b
Higor Allan <higor.allan21@gmail.com>
2022-11-26 08:30:48 -0300

    adding unit test

---
+++ packages/utils/math/sum.test.tsx
@@ -0,0 +1,13 @@
+import { sum } from "./sum";
+
+describe("sum()", () => {
+  it("when receives 1 and 1, return 2", () => {
+    expect(sum(1, 1)).toBe(2);
+  });
+  it("when receives 2 and 2, return 4", () => {
+    expect(sum(2, 2)).toBe(4);
+  });
+  it("when receives 3 and 2, return 5", () => {
+    expect(sum(3, 2)).toBe(5);
+  });
+});
