e7d4e5f5b7853f4d3531ebc4d7c0384c39596052
Higor Allan <higor.allan21@gmail.com>
2022-11-26 21:27:38 -0300

    adding verify

--- projects/web-public/pages/index.tsx
+++ projects/web-public/pages/index.tsx
@@ -1,11 +1,26 @@
 import React from "react";
 import { sum } from "@alura/utils/math/sum";
 import { Text } from "@alura/design-system/components/Text";
+import styled from "styled-components";
+
+const Main = styled.main`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  flex-direction: column;
+`;
 
 export default function Home() {
   return (
-    <main>
-      <Text tag="h1">Home soma de 2 + 2 = {sum(2, 2)}</Text>
-    </main>
+    <Main>
+      <Text tag="p">Monorepo - Design system - is Running</Text>
+      <Text tag="p">
+        Monorepo - Utils - is running if result = 4 // 2 + 2 ={" "}
+        <span style={{ color: "red" }}>{sum(2, 2)}</span>
+      </Text>
+      <Text tag="p">Setup - Test - Lint - TS - all running</Text>
+      <Text tag="p">✅✅✅✅</Text>
+    </Main>
   );
 }
